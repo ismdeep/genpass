3e039707d3a9b86543a715a358b413fb4c29d743
L. Jiang <l.jiang.1024@gmail.com>
2024-11-07 20:15:28 +0800

    feat: htpasswd

--- go.mod
+++ go.mod
@@ -5,5 +5,7 @@ go 1.16
 require (
 	github.com/google/uuid v1.6.0
 	github.com/ismdeep/args v0.0.2
+	github.com/johnaoss/htpasswd v0.0.0-20190120213328-a0cc59f788da
 	github.com/spf13/cobra v1.8.0
+	golang.org/x/crypto v0.28.0
 )
--- main.go
+++ main.go
@@ -2,10 +2,18 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"os"
 
 	"github.com/google/uuid"
+	"github.com/johnaoss/htpasswd/apr1"
 	"github.com/spf13/cobra"
+	"golang.org/x/crypto/bcrypt"
 )
+
+func init() {
+
+}
 
 func JWTCommand() *cobra.Command {
 	return &cobra.Command{
@@ -86,6 +94,67 @@ func CodeCommand() *cobra.Command {
 	}
 }
 
+func HTPasswordCommand() *cobra.Command {
+	var username string
+	var password string
+	var hashType string
+
+	m := &cobra.Command{
+		Use:   "htpasswd",
+		Short: "Generate Htpasswd Secret Key",
+		Run: func(cmd *cobra.Command, args []string) {
+
+			if username == "" {
+				fmt.Println("Please provide a username")
+				os.Exit(1)
+			}
+
+			if password == "" {
+				password = RandStr(BaseYoutubeUniqueIDAlphabet, 11)
+			}
+
+			var htpasswd string
+
+			switch hashType {
+			case "md5":
+				s, err := apr1.Hash(password, "")
+				if err != nil {
+					panic(err)
+				}
+				htpasswd = fmt.Sprintf("%v:%v", username, s)
+			case "bcrypt":
+				generateApachePassword := func(username, password string) (string, error) {
+					hashedPassword, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+					if err != nil {
+						return "", err
+					}
+					return fmt.Sprintf("%s:%s", username, hashedPassword), nil
+				}
+				apachePassword, err := generateApachePassword(username, "123456")
+				if err != nil {
+					log.Fatalf("Error generating password: %v", err)
+				}
+				htpasswd = apachePassword
+			default:
+				fmt.Println("Unsupported hash type: " + hashType)
+				os.Exit(1)
+			}
+
+			// output
+			fmt.Println("Hash Type: " + hashType)
+			fmt.Println("Username: " + username)
+			fmt.Println("Password: " + password)
+			fmt.Println("htpasswd: " + htpasswd)
+		},
+	}
+
+	m.PersistentFlags().StringVarP(&username, "username", "u", "", "Username to use for authentication")
+	m.PersistentFlags().StringVarP(&password, "password", "p", "", "Password to use for authentication")
+	m.PersistentFlags().StringVarP(&hashType, "type", "t", "md5", "Hash type")
+
+	return m
+}
+
 func MainCommand() *cobra.Command {
 	return &cobra.Command{
 		Use:           "genpass",
@@ -103,6 +172,7 @@ func main() {
 	mainCmd.AddCommand(UUIDCommand())
 	mainCmd.AddCommand(IDCommand())
 	mainCmd.AddCommand(CodeCommand())
+	mainCmd.AddCommand(HTPasswordCommand())
 	if err := mainCmd.Execute(); err != nil {
 		panic(err)
 	}
